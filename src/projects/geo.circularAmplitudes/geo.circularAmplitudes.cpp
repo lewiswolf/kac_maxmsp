f888ab0c2f25a138eab7efab1707661a6dc643a6
lewiswolf <lewiswolstanholme@gmail.com>
2022-03-29 16:40:40 +0100

    removed c74 as global namespace to have better typehints, fectored and cleaned implementations

--- src/projects/geo.circularAmplitudes/geo.circularAmplitudes.cpp
+++ src/projects/geo.circularAmplitudes/geo.circularAmplitudes.cpp
@@ -4,75 +4,78 @@
 ///	@license	Use of this source code is governed by the MIT License found in the License.md file.
 
 // core
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <vector>
 
 // dependencies
 #include "c74_min.h"
 #include "geometry.hpp"
 
-using namespace c74::min;
+namespace c = c74::min;
 namespace g = geometry;
 
-class circularAmplitudes : public object<circularAmplitudes> {
+class circularAmplitudes : public c::object<circularAmplitudes> {
 public:
-    MIN_DESCRIPTION	{"Calculate the modal amplitudes relative to a strike location in polar coordinates."};
-    MIN_TAGS		{""};
-    MIN_AUTHOR		{"Lewis Wolf"};
-    MIN_RELATED		{"geo.circularAmplitudes"};
+	MIN_DESCRIPTION	{"Calculate the modal amplitudes relative to a strike location in polar coordinates."};
+	MIN_TAGS		{""};
+	MIN_AUTHOR		{"Lewis Wolf"};
+	MIN_RELATED		{"geo.circularAmplitudes"};
 
-    inlet<>  in1	{ this, "(float) the radial component of the circular strike location" };
-    inlet<>  in2	{ this, "(float) the theta component of the circular strike location" };
-    outlet<> out	{ this, "(float) output the modal amplitudes" };
+	c::inlet<>  in1	{ this, "(float) the radial component of the circular strike location" };
+	c::inlet<>  in2	{ this, "(float) the theta component of the circular strike location" };
+	c::outlet<> out	{ this, "(float) output the modal amplitudes" };
 
 
-    argument<int> set_N { this, "N", "Update the maximum Nth order of the modes.",
-        MIN_ARGUMENT_FUNCTION { 
+	c::argument<int> set_N { this, "N", "Update the maximum Nth order of the modes.",
+		MIN_ARGUMENT_FUNCTION { 
 			N = arg;
 			series = g::calculateCircularSeries(N, M);
 		}
-    };
-    attribute<int> N { this, "N", 10,
-        description {"The maximum Nth order of the modes."}
-    };
+	};
+	c::attribute<int> N { this, "N", 10,
+		c::description {"The maximum Nth order of the modes."}
+	};
 
-	argument<int> set_M { this, "M", "Update the amount of modes per order.",
-        MIN_ARGUMENT_FUNCTION { 
+	c::argument<int> set_M { this, "M", "Update the amount of modes per order.",
+		MIN_ARGUMENT_FUNCTION { 
 			M = arg;
 			series = g::calculateCircularSeries(N, M);
 		}
-    };
-    attribute<int> M { this, "M", 10,
-        description {"The amount of modes per order."}
-    };
+	};
+	c::attribute<int> M { this, "M", 10,
+		c::description {"The amount of modes per order."}
+	};
 
 
-    message<> number { this, "number", "Calculate the modal amplitudes.",
-        MIN_FUNCTION {
-			atoms amplitudes(N * M);
-            switch (inlet) {
-                case 0:
-					r = from_atoms<std::vector<double>>(args)[0];
+	c::message<> number { this, "number", "Calculate the modal amplitudes.",
+		MIN_FUNCTION {
+			double angular;
+			c::atoms amplitudes(N * M);
+			switch (inlet) {
+				case 0:
+					r = c::from_atoms<std::vector<double>>(args)[0];
 					for (unsigned int n = 0; n < N; n++) {
+						if (n != 0) {
+							angular = M_SQRT2 * sin(n * theta + M_PI_4);
+						} else {
+							angular = 1.0;
+						} 
 						for (unsigned int m = 0; m < M; m++) {
-							double a = g::besselJ(n, series[n][m] + r);
-							if (n != 0) {
-								amplitudes[n * M + m] = a * M_SQRT2 * sin(n * theta + M_PI_4);
-							} else {
-								amplitudes[n * M + m] = a;
-							}
+							amplitudes[n * M + m] = g::besselJ(n, series[n][m] + r) * angular;
 						};	
 					}
 					out.send(amplitudes);
-                    break;
-                case 1:
-                    theta = from_atoms<std::vector<double>>(args)[0];
-                    break;
-                default:
-                    break;
-            }
+					break;
+				case 1:
+					theta = c::from_atoms<std::vector<double>>(args)[0];
+					break;
+				default:
+					break;
+			}
 			return {};
 		}
-    };
+	};
 
 
 private:
--- src/projects/geo.circularModes/geo.circularModes.cpp
+++ src/projects/geo.circularModes/geo.circularModes.cpp
@@ -7,40 +7,40 @@
 #include "c74_min.h"
 #include "geometry.hpp"
 
-using namespace c74::min;
+namespace c = c74::min;
 namespace g = geometry;
 
 
-class circularModes : public object<circularModes> {
+class circularModes : public c::object<circularModes> {
 public:
-    MIN_DESCRIPTION	{"Calculate the circular modes of vibration given a fundamental frequency."};
-    MIN_TAGS		{""};
-    MIN_AUTHOR		{"Lewis Wolf"};
-    MIN_RELATED		{"geo.circularAmplitudes"};
+	MIN_DESCRIPTION	{"Calculate the circular modes of vibration given a fundamental frequency."};
+	MIN_TAGS		{""};
+	MIN_AUTHOR		{"Lewis Wolf"};
+	MIN_RELATED		{"geo.circularAmplitudes"};
 
-    inlet<>  in		{ this, "(float) convert fundamental frequency to circular modes." };
-    outlet<> out	{ this, "(float) output the circular modes." };
-
-
-    argument<int> set_N { this, "N", "Update the maximum Nth order of the modes.",
-        MIN_ARGUMENT_FUNCTION { N = arg; }
-    };
-    attribute<int> N { this, "N", 10,
-        description {"The maximum Nth order of the modes."}
-    };
-
-	argument<int> set_M { this, "M", "Update the amount of modes per order.",
-        MIN_ARGUMENT_FUNCTION { M = arg; }
-    };
-    attribute<int> M { this, "M", 10,
-        description {"The amount of modes per order."}
-    };
+	c::inlet<>  in	{ this, "(float) convert fundamental frequency to circular modes." };
+	c::outlet<> out	{ this, "(float) output the circular modes." };
 
 
-    message<> number { this, "number", "Calculate the modal frequencies.",
-        MIN_FUNCTION {
+	c::argument<int> set_N { this, "N", "Update the maximum Nth order of the modes.",
+		MIN_ARGUMENT_FUNCTION { N = arg; }
+	};
+	c::attribute<int> N { this, "N", 10,
+		c::description {"The maximum Nth order of the modes."}
+	};
+
+	c::argument<int> set_M { this, "M", "Update the amount of modes per order.",
+		MIN_ARGUMENT_FUNCTION { M = arg; }
+	};
+	c::attribute<int> M { this, "M", 10,
+		c::description {"The amount of modes per order."}
+	};
+
+
+	c::message<> number { this, "number", "Calculate the modal frequencies.",
+		MIN_FUNCTION {
 			auto modes_old = g::calculateCircularModes(args[0], N, M);
-            atoms modes(N * M);
+			c::atoms modes(N * M);
 			for (unsigned int i = 0; i < N; i++) {
 				for (unsigned int j = 0; j < M; j++) {
 					modes[i * M + j] = modes_old[i][j];
@@ -49,7 +49,7 @@ public:
 			out.send(modes);
 			return {};
 		}
-    };
+	};
 };
 
 MIN_EXTERNAL(circularModes);
