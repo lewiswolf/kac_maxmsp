d69c30dcc4c587e6b8a079f4d0797f268cd1f7b4
lewiswolf <lewiswolstanholme@gmail.com>
2023-01-17 15:19:28 +0000

    added sqaureToCircle

---
+++ src/projects/kac.squareToCircle/kac.squareToCircle.cpp
@@ -0,0 +1,53 @@
+// core
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <vector>
+
+// dependencies
+#include "c74_min.h"
+#include <kac_core.hpp>
+
+namespace c = c74::min;
+namespace g = kac_core::geometry;
+
+class squareToCircle : public c::object<squareToCircle> {
+public:
+	MIN_DESCRIPTION	{"Map coordinates from a square to a circle."};
+	MIN_TAGS		{""};
+	MIN_AUTHOR		{"Lewis Wolstanholme"};
+	MIN_RELATED		{""};
+
+	c::inlet<>  in1	{this, "(float) the x component of the square strike location." };
+	c::inlet<>  in2	{this, "(float) the y component of the square strike location." };
+	c::outlet<> out1 {this, "(float) the x component output the mapped coordinates." };
+	c::outlet<> out2 {this, "(float) the y component output the mapped coordinates." };
+
+	c::message<> number {this, "number", "Map coordinates from a square to a circle.",
+		MIN_FUNCTION {
+			// update x and y
+			switch (inlet) {
+				case 0:
+					x = c::from_atoms<std::vector<double>>(args)[0];
+					break;
+				case 1:
+					y = c::from_atoms<std::vector<double>>(args)[0];
+					return {};
+				default:
+					return {};
+			}
+
+			// calculate coordinates when x is updated
+			c::atoms coordinates(2);
+			T::Point p = g::simpleElliptic_Square2Circle(T::Point(x, y));
+			out1.send(p.x);
+			out2.send(p.y);
+			return {};
+		}
+	};
+
+private:
+	double x;
+	double y;
+};
+
+MIN_EXTERNAL(squareToCircle);
