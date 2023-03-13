db6304ab5ff977bc3697107ba6d211a132b0bc11
lewiswolf <lewiswolstanholme@gmail.com>
2023-03-13 02:02:36 +0000

    added generateIrregularPolygon

---
+++ src/projects/kac.generateIrregularStar/kac.generateIrregularStar.cpp
@@ -0,0 +1,40 @@
+// core
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <vector>
+
+// dependencies
+#include "c74_min.h"
+#include <kac_core.hpp>
+
+namespace c = c74::min;
+namespace g = kac_core::geometry;
+namespace T = kac_core::types;
+
+class generateIrregularStar : public c::object<generateIrregularStar> {
+public:
+	MIN_DESCRIPTION {"Randomly generate a polygon."};
+	MIN_TAGS {""};
+	MIN_AUTHOR {"Lewis Wolstanholme"};
+	MIN_RELATED {"kac.normalisePolygon"};
+
+	c::inlet<> in1 {this, "(int) the number of vertices."};
+	c::outlet<> out {this, "(list) output the vertices of the polygon."};
+
+	c::message<> number {this, "number", "Generate a polygon with n vertices.",
+		MIN_FUNCTION {
+			int N = c::from_atoms<std::vector<double>>(args)[0];
+			if (N < 3) { return {}; }
+			T::Polygon polygon_old = g::generateIrregularStar(N);
+			c::atoms polygon(N * 2);
+			for (unsigned int i = 0; i < N; i++) {
+				polygon[2 * i] = polygon_old[i].x;
+				polygon[2 * i + 1] = polygon_old[i].y;
+			}
+			out.send(polygon);
+			return {};
+		}
+	};
+};
+
+MIN_EXTERNAL(generateIrregularStar);
